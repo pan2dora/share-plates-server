7c051494e244ade65fa5e7e7956ad0fca3276027
Pandora <pan2dora.brown@gmail.com>
2025-05-26 15:50:20 -0400

    Added middleware handling

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 const express = require("express");
-
+const morgan = require("morgan");
+const helmet = require("helmet");
+const cors = require("cors");
+const path = require("node:path")
 const app = express();
 
-const PORT = 3000;
+const PORT = 8080;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan("combined"));
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.get("/", (req, res, next) => {
   res.send("This is the home page");
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is listening on http://localhost:${PORT}/`);
+  console.log(
+    `Server is listening on http://localhost:${PORT}. Connection established`
+  );
 });
