2142af9d1b29ca307fba1693393bcc163bbe0f13
Pandora <pan2dora.brown@gmail.com>
2025-06-22 14:54:34 -0400

    fixed locl login route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -24,7 +24,7 @@ router.get("/login/error", (req, res, next) => {
   return res.json("Login Error!");
 });
 
-router.get("/login/local", localLogin);
+router.post("/login/local", localLogin);
 router.get("/logout", logout);
 //Auth
 router.get(
